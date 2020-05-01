a31206ab99e5a6e224d434d34241b810f129d6cc
Keshav Mathur <keshav37a@gmail.com>
2020-05-01 12:45:06 +0530

    changes in state

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,10 +6,15 @@ class CartItem extends React.Component{
         this.state = {
             title: 'Roasted Chicken',
             price: 500,
-            quantity: '1 kg',
+            quantity: 1,
             img: ''
         }
     }
+
+    increaseQty = ()=>{
+        console.log('this', this.state.quantity);
+    }
+
     render(){
         const {title, price, quantity, img} = this.state;
         return (
@@ -18,15 +23,19 @@ class CartItem extends React.Component{
                     <img alt="item-img" style={styles.image}></img>
                 </div>
                 <div className="right-block">
-        <div style={{fontSize : 25, fontWeight:600}}> {title} </div>
-        <div style={{color:'#777'}}>{price}</div>
-        <div style={{color:'#777'}}>Qty:{quantity}</div>
+
+                    <div style={{fontSize : 25, fontWeight:600}}> {title} </div>
+                    <div style={{color:'#777'}}>{price}</div>
+                    <div style={{color:'#777'}}>Qty:{quantity} kg</div>
+
                     <div className="cart-item-actions">
-                        <img alt="increase-quantity" src="https://image.flaticon.com/icons/svg/1828/1828926.svg" className="action-icons"></img>
+
+                        <img alt="increase-quantity" src="https://image.flaticon.com/icons/svg/1828/1828926.svg" className="action-icons" onClick={this.increaseQty}></img>
 
                         <img alt="decrease-quantity" src="https://image.flaticon.com/icons/svg/1828/1828906.svg" className="action-icons"></img>
 
                         <img alt="remove-item" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" className="action-icons"></img>
+
                     </div>
                 </div>
             </div>
